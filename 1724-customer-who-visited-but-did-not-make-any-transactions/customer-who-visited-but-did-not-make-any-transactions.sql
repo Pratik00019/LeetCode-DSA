89ce8c49461b854c321b0bced748b865ba3c02d5
Pratik Padalkar <62217310+Pratik00019@users.noreply.github.com>
2026-05-19 00:02:17 +0530

    Time: 1124 ms (89.40%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,12 +1,21 @@
 /* Write your T-SQL query statement below */
 
-SELECT customer_id, COUNT(*) AS count_no_trans 
+-- SELECT customer_id, COUNT(*) AS count_no_trans 
+-- FROM Visits v
+-- WHERE NOT EXISTS (
+--     SELECT 1 
+--     FROM Transactions t
+--     WHERE v.visit_id=t.visit_id
+-- )
+-- GROUP BY v.customer_id
+-- ORDER BY COUNT(*) DESC
+
+
+SELECT v.customer_id,COUNT(*) as count_no_trans 
 FROM Visits v
-WHERE NOT EXISTS (
-    SELECT 1 
-    FROM Transactions t
-    WHERE v.visit_id=t.visit_id
-)
+LEFT JOIN Transactions t
+ON t.visit_id=v.visit_id
+WHERE t.visit_id is NULL
 GROUP BY v.customer_id
-ORDER BY COUNT(*) DESC
+
 
