9df601629f6117c275e7460635daf877f51231f3
Pratik Padalkar <62217310+Pratik00019@users.noreply.github.com>
2026-06-06 11:56:40 +0530

    Time: 504 ms (88.49%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,8 +1,18 @@
 /* Write your T-SQL query statement below */
-SELECT DISTINCT l1.num as ConsecutiveNums 
-FROM Logs l1
-JOIN Logs l2
-ON l1.id+1=l2.id
-JOIN Logs l3
-ON l2.id+1=l3.id
-WHERE l1.num=l2.num AND l2.num=l3.num
+-- SELECT DISTINCT l1.num as ConsecutiveNums 
+-- FROM Logs l1
+-- JOIN Logs l2
+-- ON l1.id+1=l2.id
+-- JOIN Logs l3
+-- ON l2.id+1=l3.id
+-- WHERE l1.num=l2.num AND l2.num=l3.num
+
+WITH Temp AS
+(
+    SELECT id, num,LAG(num,1) OVER (ORDER BY id) as consecutive1,LAG(num,2) OVER (ORDER BY id) as consecutive2
+    FROM LOGS
+)
+
+SELECT DISTINCT (Temp.num) as ConsecutiveNums
+FROM Temp
+WHERE temp.consecutive2=Temp.num AND temp.consecutive1=Temp.num
