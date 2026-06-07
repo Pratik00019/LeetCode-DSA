e3984fa175ee5052957ed5ea8a1881644188af08
Pratik Padalkar <62217310+Pratik00019@users.noreply.github.com>
2026-06-07 16:14:47 +0530

    Time: 1533 ms (10.01%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,7 +1,15 @@
-SELECT TOP 1 q1.person_name
-FROM Queue q1
-JOIN Queue q2
-ON q1.turn >= q2.turn
-GROUP BY q1.turn, q1.person_name
-HAVING SUM(q2.weight) <= 1000
-ORDER BY SUM(q2.weight) DESC;
+-- SELECT TOP 1 q1.person_name
+-- FROM Queue q1
+-- JOIN Queue q2
+-- ON q1.turn >= q2.turn
+-- GROUP BY q1.turn, q1.person_name
+-- HAVING SUM(q2.weight) <= 1000
+-- ORDER BY SUM(q2.weight) DESC;
+
+WITH Temp as (
+    SELECT person_name , SUM(weight) OVER (ORDER BY turn) as sum FROM Queue
+)
+
+SELECT TOP 1 t.person_name FROM Temp t
+WHERE t.sum<=1000
+ORDER BY t.sum DESC
