2a9174a7b5e8cbd54764c64d1e0773ce085b6fbc
Pratik Padalkar <62217310+Pratik00019@users.noreply.github.com>
2026-06-12 20:43:16 +0530

    Time: 418 ms (27.23%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,18 @@
 /* Write your T-SQL query statement below */
-SELECT MAX(salary) as SecondHighestSalary
-FROM Employee
-WHERE Salary != (
-    SELECT MAX(salary) FROM Employee
-)
+-- SELECT MAX(salary) as SecondHighestSalary
+-- FROM Employee
+-- WHERE Salary < (
+--     SELECT MAX(salary) FROM Employee
+-- )
+
+SELECT MAX(SecondHighestSalary) AS SecondHighestSalary
+FROM
+(
+    SELECT salary AS SecondHighestSalary,
+           DENSE_RANK() OVER
+           (
+               ORDER BY salary DESC
+           ) AS rn
+    FROM Employee
+) t
+WHERE rn = 2;
