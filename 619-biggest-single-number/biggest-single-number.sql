21480f7e6d577754affba9025d1c6623f25c69d5
Pratik Padalkar <62217310+Pratik00019@users.noreply.github.com>
2026-06-13 12:23:26 +0530

    Time: 653 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
 WITH Temp AS 
-    (SELECT num,COUNT(*) as count FROM MyNumbers GROUP BY num) 
+    (SELECT num,COUNT(*) as count FROM MyNumbers GROUP BY num HAVING (COUNT(*)=1)) 
 
-SELECT MAX(t.num) as num FROM MyNumbers n
-JOIN TEMP t
-ON t.num=n.num AND t.count=1
+SELECT MAX(num) as num FROM Temp
